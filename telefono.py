143c026f40d24a6c01338c06e4617ed07f9ee671
terranigmark <linnk99@gmail.com>
2024-10-15 19:21:00 -0600

    Se creo la clase Telefono y su metodo init

---
+++ telefono.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+
+class Telefono():
+    def __init__(self, fabricante, modelo, camara, pantalla):
+        self.fabricante = fabricante
+        self.modelo = modelo
+        self.camara = camara
+        self.pantalla = pantalla
